f0d926eb27c960fb73b7b3d59a1fad241a75d3f0
Moaath Alrajab <moaath@alrajab.net>
2022-12-31 15:47:07 -0500

    Initial Commit

---
+++ src/main/java/com/mycompany/studentlst/Student.java
@@ -0,0 +1,17 @@
+package com.mycompany.studentlst;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class Student {
+    
+    private String name;
+
+    //To Do 01: add a constructor to this class 
+    
+    // To Do 02: Add setters and getters
+    
+    
+    
+}
