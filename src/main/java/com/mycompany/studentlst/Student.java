0d30d558b35595f3f0b111c79b830074a4f84a4d
nafisa <nafisa@Nafisas-MacBook-Pro.local>
2023-09-25 12:50:45 -0400

    To Do 1 and 2

--- src/main/java/com/mycompany/studentlst/Student.java
+++ src/main/java/com/mycompany/studentlst/Student.java
@@ -8,12 +8,18 @@ public class Student {
     
     private String name;
 
-    // new comment
+    //To Do 01: add a constructor to this class
+    public Student(String name) {
+        this.name = name;
 
-    //To Do 01: add a constructor to this class 
+    }
     
     // To Do 02: Add setters and getters
-    
-    
-    
+    public String getName() {
+        return name;
+    }
+     public void setName(String name) {
+        this.name = name;
+     }
+
 }
