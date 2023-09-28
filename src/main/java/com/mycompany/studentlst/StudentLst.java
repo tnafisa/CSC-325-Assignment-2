5be91818c23cfc3f2c97223b707444d9fb7af6a9
nafisa <nafisa@Nafisas-MacBook-Pro.local>
2023-09-28 11:09:27 -0400

    Second Commit

--- src/main/java/com/mycompany/studentlst/StudentLst.java
+++ src/main/java/com/mycompany/studentlst/StudentLst.java
@@ -23,11 +23,15 @@ public class StudentLst {
     }
     
     public static String printOutput(Student[] students){
-        String str="";
-        
+        String str= new String();
+        str.append("Student List:\n");
+
+        for (int i = 0; i < students.lenghts; i++) str.append("Student ").append(i + 1).append(": ");
+
         //To Do 03: complete the implementation of this method  
-        
-        return str;
+
+        return str.String();
+        return string;
     }
     
     
